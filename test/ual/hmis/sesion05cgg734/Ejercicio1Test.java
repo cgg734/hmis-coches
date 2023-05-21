342fbb754d1333ef372c987f0084ab152c8e3452
Cristian Gil García <cgg734@inlumine.ual.es>
2023-05-21 20:08:07 +0200

    test de ejercicio1

---
+++ test/ual/hmis/sesion05cgg734/Ejercicio1Test.java
@@ -0,0 +1,26 @@
+package ual.hmis.sesion05cgg734;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Ejercicio1Test {
+
+	@Test
+    public void testTransformar() {
+        Ejercicio1 ejercicio = new Ejercicio1();
+
+        // Prueba con un número par
+        assertEquals(0, ejercicio.transformar(8));
+
+        // Prueba con un número divisible por 3
+        assertEquals(0, ejercicio.transformar(9));
+
+        // Prueba con un número divisible por 5
+        assertEquals(0, ejercicio.transformar(25));
+
+        // Prueba con un número que no es divisible por 2, 3 ni 5
+        assertEquals(7, ejercicio.transformar(7));
+    }
+
+}
