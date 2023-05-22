e0758401c6200eb14d3683b3f0dde145c7e58cda
Cristian Gil García <cgg734@inlumine.ual.es>
2023-05-22 12:31:26 +0200

    Arreglo 2

--- src/ual/hmis/sesion05cgg734/Ejercicio2.java
+++ src/ual/hmis/sesion05cgg734/Ejercicio2.java
@@ -3,17 +3,46 @@ package ual.hmis.sesion05cgg734;
 public class Ejercicio2 {
 	
 	public boolean login(String username, String password) {
-		// comprobar que sean distintos de vacio
+		
+	    // Comprobar que sean distintos de vacío
 		if (username.isEmpty() || password.isEmpty()) {
-			return false;
-		}
-		// comprobar que la longitud sea < 30
-		else if (username.length() > 30 || password.length() > 30) {
-			return false;
-		}
-		// llamar al método de la bbdd
-		else
-			return compruebaLoginEnBD(username, password);
+	        return false;
+	    }
+	    // Comprobar que la longitud sea < 30
+	    else if (username.length() > 30 || password.length() > 30) {
+	        return false;
+	    }
+	    // Comprobar que contiene una mayúscula, minúscula y dígito
+	    else if (contieneMayusculaMinusculaDigito(password)) {
+	    	return true;
+	    }
+	    else
+	    	return compruebaLoginEnBD(username, password);
+	}
+	
+	public boolean contieneMayusculaMinusculaDigito(String password) {
+	    boolean contieneMayuscula = false;
+	    boolean contieneMinuscula = false;
+	    boolean contieneDigito = false;
+
+	    for (int i = 0; i < password.length(); i++) {
+	        char c = password.charAt(i);
+
+	        if (Character.isUpperCase(c)) {
+	            contieneMayuscula = true;
+	        } else if (Character.isLowerCase(c)) {
+	            contieneMinuscula = true;
+	        } else if (Character.isDigit(c)) {
+	            contieneDigito = true;
+	        }
+
+	        // Si ya se cumple la condición de tener una mayúscula, una minúscula y un dígito, podemos salir del bucle
+	        if (contieneMayuscula && contieneMinuscula && contieneDigito) {
+	            break;
+	        }
+	    }
+
+	    return contieneMayuscula && contieneMinuscula && contieneDigito;
 	}
 
 	public boolean compruebaLoginEnBD(String username, String password) {
--- test/ual/hmis/sesion05cgg734/Ejercicio2Test.java
+++ test/ual/hmis/sesion05cgg734/Ejercicio2Test.java
@@ -8,44 +8,115 @@ class Ejercicio2Test {
 
 	@Test
     public void testLogin() {
-		Ejercicio2 login = new Ejercicio2();
+        Ejercicio2 ejercicio2 = new Ejercicio2();
 
-		// Prueba con datos de inicio de sesión válidos
-		assertTrue(login.login("user", "pass"));
-		
-		// Prueba con datos de inicio de sesión válidos
-        assertTrue(login.compruebaLoginEnBD("user", "pass"));
+        // Caso de prueba: username y password válidos
+        assertTrue(ejercicio2.login("user", "pass"));
 
-        // Prueba con datos de inicio de sesión inválidos (nombre de usuario incorrecto)
-        assertFalse(login.compruebaLoginEnBD("invalid", "pass"));
+        // Caso de prueba: username vacío
+        assertFalse(ejercicio2.login("", "pass"));
 
-        // Prueba con datos de inicio de sesión inválidos (contraseña incorrecta)
-        assertFalse(login.compruebaLoginEnBD("user", "invalid"));
+        // Caso de prueba: password vacío
+        assertFalse(ejercicio2.login("user", ""));
 
-        // Prueba con datos de inicio de sesión inválidos (nombre de usuario y contraseña incorrectos)
-        assertFalse(login.compruebaLoginEnBD("invalid", "invalid"));
+        // Caso de prueba: username y password vacíos
+        assertFalse(ejercicio2.login("", ""));
 
-		// Prueba con datos de inicio de sesión inválidos
-		assertFalse(login.login("invalid", "password"));
+        // Caso de prueba: username y password con longitud mayor a 30
+        assertFalse(ejercicio2.login("thisIsAUsernameWithMoreThan30Characters", "ThisIsAPasswordWithMoreThan30Characters"));
 
-		// Prueba con un nombre de usuario vacío
-		assertFalse(login.login("", "password"));
+        // Caso de prueba: password sin mayúscula, minúscula y dígito
+        assertFalse(ejercicio2.login("user", "password"));
 
-		// Prueba con una contraseña vacía
-		assertFalse(login.login("user", ""));
+        // Caso de prueba: username y password válidos, pero falla en la comprobación de la base de datos
+        assertFalse(ejercicio2.login("user", "pass123"));
 
-		// Prueba con datos de inicio de sesión inválidos (nombre de usuario con más de
-		// 30 caracteres)
-		assertFalse(login.login("thisIsAUsernameWithMoreThan30Characters", "pass"));
+        // Caso de prueba: username y password válidos, y éxito en la comprobación de la base de datos
+        assertTrue(ejercicio2.login("user", "pass"));
+        
+        
+     // Caso de prueba: username y password con longitud menor a 30
+        assertTrue(ejercicio2.login("user", "pass"));
 
-		// Prueba con datos de inicio de sesión inválidos (contraseña con más de 30
-		// caracteres)
-		assertFalse(login.login("user", "thisIsAPasswordWithMoreThan30Characters"));
+        // Caso de prueba: username con longitud mayor a 30
+        assertFalse(ejercicio2.login("usernameusernameusernameusernameusername", "pass"));
 
-		// Prueba con datos de inicio de sesión inválidos (nombre de usuario y
-		// contraseña con más de 30 caracteres)
-		assertFalse(login.login("thisIsAUsernameWithMoreThan30Characters", "thisIsAPasswordWithMoreThan30Characters"));
+        // Caso de prueba: password con longitud mayor a 30
+        assertFalse(ejercicio2.login("user", "passwordpasswordpasswordpasswordpassword"));
 
-	}
+        // Caso de prueba: password sin mayúscula, minúscula y dígito
+        assertFalse(ejercicio2.login("user", "password"));
+
+        // Caso de prueba: password solo con mayúscula
+        assertFalse(ejercicio2.login("user", "PASSWORD"));
+
+        // Caso de prueba: password solo con minúscula
+        assertFalse(ejercicio2.login("user", "password"));
+
+        // Caso de prueba: password solo con dígito
+        assertFalse(ejercicio2.login("user", "123456"));
+
+        // Caso de prueba: password con mayúscula, minúscula y dígito
+        assertTrue(ejercicio2.login("user", "Password123"));
+    
+    }
+	
+	
+	@Test
+    public void testContieneMayusculaMinusculaDigito() {
+        Ejercicio2 contieneMayusculaMinusculaDigito = new Ejercicio2();
+
+        // Caso de prueba: cadena vacía
+        assertFalse(contieneMayusculaMinusculaDigito.contieneMayusculaMinusculaDigito(""));
+
+        // Caso de prueba: cadena sin mayúscula, sin minúscula y sin dígito
+        assertFalse(contieneMayusculaMinusculaDigito.contieneMayusculaMinusculaDigito("password"));
+
+        // Caso de prueba: cadena con una mayúscula
+        assertFalse(contieneMayusculaMinusculaDigito.contieneMayusculaMinusculaDigito("Password"));
+
+        // Caso de prueba: cadena con una minúscula
+        assertFalse(contieneMayusculaMinusculaDigito.contieneMayusculaMinusculaDigito("PASSWORD"));
+
+        // Caso de prueba: cadena con un dígito
+        assertFalse(contieneMayusculaMinusculaDigito.contieneMayusculaMinusculaDigito("12345"));
+
+        // Caso de prueba: cadena con una mayúscula, una minúscula y un dígito
+        assertTrue(contieneMayusculaMinusculaDigito.contieneMayusculaMinusculaDigito("Password1"));
+
+        // Caso de prueba: cadena con una mayúscula, una minúscula y un dígito, pero en diferente orden
+        assertTrue(contieneMayusculaMinusculaDigito.contieneMayusculaMinusculaDigito("1pAssword"));
+
+        // Caso de prueba: cadena con más de una mayúscula, más de una minúscula y más de un dígito
+        assertTrue(contieneMayusculaMinusculaDigito.contieneMayusculaMinusculaDigito("Pass1Word"));
+        
+        //Contiene dígito
+        assertFalse(contieneMayusculaMinusculaDigito.contieneMayusculaMinusculaDigito("Aa"));
+        
+        //todo minúscula y dígito
+        assertFalse(contieneMayusculaMinusculaDigito.contieneMayusculaMinusculaDigito("aghd2"));
+        
+    }
+	
+	
+	@Test
+    public void testCompruebaLoginEnBD() {
+        Ejercicio2 compruebaLoginEnBD = new Ejercicio2();
+
+        // Caso de prueba: username y password válidos
+        assertTrue(compruebaLoginEnBD.compruebaLoginEnBD("user", "pass"));
+
+        // Caso de prueba: username y password válidos pero en diferente orden
+        assertFalse(compruebaLoginEnBD.compruebaLoginEnBD("pass", "user"));
+
+        // Caso de prueba: username válido pero password inválido
+        assertFalse(compruebaLoginEnBD.compruebaLoginEnBD("user", "password"));
+
+        // Caso de prueba: username inválido pero password válido
+        assertFalse(compruebaLoginEnBD.compruebaLoginEnBD("username", "pass"));
+
+        // Caso de prueba: username y password inválidos
+        assertFalse(compruebaLoginEnBD.compruebaLoginEnBD("username", "password"));
+    }
 
 }
